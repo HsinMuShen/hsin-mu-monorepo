6b7ac5bcfe8241ef9ec9c45154aec5358f37c05d
michael_shen <electrycity84@gmail.com>
2024-12-15 16:13:10 +0800

    feat: use ui in react-monorepo

--- apps/react-monorepo/src/app/app.tsx
+++ apps/react-monorepo/src/app/app.tsx
@@ -1,10 +1,12 @@
 import NxWelcome from './nx-welcome';
 import { Ui } from '@react-monorepo/ui';
+import Button from '@mui/material/Button';
 
 export function App() {
   return (
     <div>
       <Ui />
+      <Button variant="contained">Hello World</Button>
       <h1 className="text-4xl font-bold text-blue-500">Hello World</h1>
       <NxWelcome title="react-monorepo" />
     </div>
